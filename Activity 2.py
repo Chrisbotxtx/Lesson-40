74a592c972f1cb42e6a208fe287a63716568034c
Chrisbotxtx <christopher.mwakasegecmj@gmail.com>
2024-11-08 19:48:33 +0300

    Lesson 40

---
+++ Activity 2.py
@@ -0,0 +1,6 @@
+
+n=int(input("Enter desired number of rows"))
+for i in range(1,n+1):
+    for p in range(i):
+        print("*",end='')
+    print()
---
+++ Activity 3.py
@@ -0,0 +1,7 @@
+j=0
+sum=0
+n=int(input("Enter a number"))
+while j<n:
+    sum=sum+j
+    j=j+1
+print("Sum of",n,"is",sum)
